bd548272b65fcb23bf43dd53745df2faf1477508
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2025-12-17 19:05:05 +0530

    Fixed cors error

--- server/server.js
+++ server/server.js
@@ -26,10 +26,22 @@ const allowedOrigins = (process.env.CLIENT_URL || '')
 
 app.use(
   cors({
-    origin: "https://eventhub-sooty-nine.vercel.app",
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
-  }),
+    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+    allowedHeaders: ['Content-Type','Authorization'],
+  })
 );
+
+app.options('*', cors());
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
